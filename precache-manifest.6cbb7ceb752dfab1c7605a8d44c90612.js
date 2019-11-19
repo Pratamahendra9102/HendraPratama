415d078f535c6cd1270f19a19c9745d479bd3b2d
unknown <pratamahendra9102@gmail.com>
2019-11-19 17:38:54 +0700

    Updates

---
+++ precache-manifest.6cbb7ceb752dfab1c7605a8d44c90612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0563cf5d8fb881a0b51ce22aeb0e3b",
+    "url": "/HendraPratama/index.html"
+  },
+  {
+    "revision": "1d5d0a3ea9276b3f4bd1",
+    "url": "/HendraPratama/static/css/main.85096027.chunk.css"
+  },
+  {
+    "revision": "603e853593a67b3fb939",
+    "url": "/HendraPratama/static/js/2.f6aed957.chunk.js"
+  },
+  {
+    "revision": "1d5d0a3ea9276b3f4bd1",
+    "url": "/HendraPratama/static/js/main.2f5e6b22.chunk.js"
+  },
+  {
+    "revision": "978cf25972f47b303319",
+    "url": "/HendraPratama/static/js/runtime-main.cd071f60.js"
+  }
+]);
